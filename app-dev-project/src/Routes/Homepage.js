5ffe5eaf1f093b0c72e11c1391d96273288377cf
Hayden McDowall (1000051359) <MCDOHR2@student.op.ac.nz>
2022-05-25 14:56:32 +1200

    Added template for games on the homepage. Rough layout

--- app-dev-project/src/Routes/Homepage.js
+++ app-dev-project/src/Routes/Homepage.js
@@ -2,8 +2,53 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div className="Homepage">
-      
+    <div className="container">
+      <div className="gameContainer">
+        <div className="game">
+        <div>Game room name</div>
+          <div>Players:</div>
+          <div>Player 1</div>
+          <div>Player 2</div>
+          <div>Player 3</div>
+          <button>Join</button>
+        </div>
+
+        <div className="game">
+        <div>Game room name</div>
+          <div>Players:</div>
+          <div>Player 1</div>
+          <div>Player 2</div>
+          <div>Player 3</div>
+          <button>Join</button>
+        </div>
+
+        <div className="game">
+        <div>Game room name</div>
+          <div>Players:</div>
+          <div>Player 1</div>
+          <div>Player 2</div>
+          <div>Player 3</div>
+          <button>Join</button>
+        </div>
+
+        <div className="game">
+        <div>Game room name</div>
+          <div>Players:</div>
+          <div>Player 1</div>
+          <div>Player 2</div>
+          <div>Player 3</div>
+          <button>Join</button>
+        </div>
+        
+        <div className="game">
+          <div>Game room name</div>
+          <div>Players:</div>
+          <div>Player 1</div>
+          <div>Player 2</div>
+          <div>Player 3</div>
+          <button>Join</button>
+        </div>
+      </div>
     </div>
   );
 };
