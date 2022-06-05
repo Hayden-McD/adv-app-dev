39d6ca0a9393ffb3ab60f9f9e130f610ed67ecb8
Hayden McDowall (1000051359) <MCDOHR2@student.op.ac.nz>
2022-06-05 18:25:10 +1200

    Added firestore and hosted app

--- app-dev-project/src/firebase.js
+++ app-dev-project/src/firebase.js
@@ -1,7 +1,7 @@
 import 'firebase/auth'
-
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,8 +14,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const fire = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(fire)
+export const auth = getAuth(app);
 
-export default fire;
+const db = getFirestore(app);
+
+export default {app, db};
