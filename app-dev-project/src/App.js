c89083da34849e55b860323b5aca94bc4d8e2070
Hayden McDowall (1000051359) <MCDOHR2@student.op.ac.nz>
2022-06-06 19:22:19 +1200

    Trying to get app to remember user when page is refreshed

--- app-dev-project/src/App.js
+++ app-dev-project/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect  } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect, useCallback } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./CSS/App.css";
 import { routes } from "./Routes/routePaths";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AuthProvider } from './Components/AuthContext'
-import { auth } from './firebase';
-import { onAuthStateChanged } from 'firebase/auth';
+import { AuthProvider } from "./Components/AuthContext";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 // Pages
 import Homepage from "./Routes/Homepage";
@@ -18,55 +18,70 @@ import Error404Page from "./Routes/Error404Page";
 // Components
 import Navbar from "./Components/NavbarComponent";
 
-
-
-const App = () =>  {
+const App = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, user => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
+        console.log("There is a user")
         setUser({
           uid: user.uid,
           email: user.email,
-          name: '',
-          loginProvider: user.providerId
-        })
-      }
-      else {
+          name: "",
+          loginProvider: user.providerId,
+        });
+      } else {
+        console.log("There isnt a user")
         setUser(null);
       }
-    })
-    return (() => unsubscribe())
-  }, [auth])
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+
+  useCallback(() => {
+    
+  })
 
   return (
     <Router>
       <AuthProvider>
         <Navbar />
         <Switch>
+          {/* Route for sign up page */}
           <Route exact path={routes.SIGNUP}>
-          <SignupPage user={user}/>
+            <SignupPage user={user} />
           </Route>
-          <Route exact path={routes.HOME}>
-          <Homepage user={user}/>
-          </Route>
+
+          {/* Route for home page */}
           <Route exact path={routes.LOGIN}>
-          <LoginPage user={user}/>
+            <LoginPage user={user} />
           </Route>
+
+          {/* Route for the how to play page */}
+          <Route exact path={routes.HOME}>
+            <Homepage user={user} />
+          </Route>
+
+          {/* Route for log in page */}
           <Route exact path={routes.HOWTOPLAY}>
-          <HowToPlayPage user={user}/>
+            <HowToPlayPage user={user} />
           </Route>
+
+          {/* Route for the user profile page */}
           <Route exact path={routes.PROFILE}>
-          <ProfilePage user={user}/>
+            <ProfilePage user={user} />
           </Route>
+
+          {/* Route for the 404 error page */}
           <Route>
-          <Error404Page/>
+            <Error404Page />
           </Route>
         </Switch>
       </AuthProvider>
     </Router>
   );
-}
+};
 
 export default App;
--- app-dev-project/src/Components/NavbarComponent.js
+++ app-dev-project/src/Components/NavbarComponent.js
@@ -9,7 +9,7 @@ const NavbarComponent = ({ user }) => {
       <Container>
         <Navbar.Brand href={routes.HOME}>PokerProject</Navbar.Brand>
         {user ? (
-          //User must be logged in to see these links in the navbar
+          /* User must be logged in to see these links in the navbar */
           <Navbar.Collapse>
             <Nav className="me-auto">
               <Nav.Link href={routes.HOME}>Games</Nav.Link>
@@ -20,15 +20,15 @@ const NavbarComponent = ({ user }) => {
               <LogoutButton />
             </Nav>
           </Navbar.Collapse>
-        ) : (
-          // User must be logged out to see these links in the navbar
+          ) : ( 
+          /* User must be logged out to see these links in the navbar */
           <Navbar.Collapse>
             <Nav>
               <Nav.Link href={routes.LOGIN}>login</Nav.Link>
               <Nav.Link href={routes.SIGNUP}>signup</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-        )}
+         )} 
       </Container>
     </Navbar>
   );
