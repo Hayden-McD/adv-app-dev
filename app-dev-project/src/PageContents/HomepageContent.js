93a2aee179502b7f1892b5820eb7d4c9fb106e59
Anthony Legg <leggtc1@student.op.ac.nz>
2022-06-24 13:28:06 +1200

    remove history from homepage component

--- app-dev-project/src/PageContents/HomepageContent.js
+++ app-dev-project/src/PageContents/HomepageContent.js
@@ -3,55 +3,50 @@ import { collection, getDocs, query } from 'firebase/firestore';
 import { db } from '../firebase';
 import Game from '../Components/Game';
 import LoadingPage from '../Routes/LoadingPage';
-import { useHistory } from 'react-router-dom';
 
 const HomepageContent = ({ authError, isLoggedIn, user, auth }) => {
     const [games, setGames] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [gamesReady, setGamesReady] = useState(false);
     const [loadError, setLoadError] = useState(null);
-    const history = useHistory();
 
     const getGame = useCallback(async () => {
         let gamesArray = [];
         const q = query(collection(db, 'Games'));
         await getDocs(q)
-            .then((res) => {
-                res.forEach((doc) => {
-                    gamesArray.push({ ...doc.data(), id: doc.id });
-                });
-                setLoadError(null);
-            }, 
-            (error) => {
-                throw new error('Game Loading Error.')
-            })
+            .then(
+                (res) => {
+                    res.forEach((doc) => {
+                        gamesArray.push({ ...doc.data(), id: doc.id });
+                    });
+                    setLoadError(null);
+                },
+                (error) => {
+                    throw new error('Game Loading Error.');
+                }
+            )
             .catch((err) => {
                 console.log(err);
                 setLoadError(err.message);
-            })
+            });
         await setGames(gamesArray);
         setIsLoading(false);
         return;
     }, []);
 
     useEffect(() => {
-        if(!isLoggedIn){
-            history.replace('/login');
-        }
-    }, []);
-    
-    useEffect(() => {
-        getGame().then(() => {
-            if(games.length > 0) {
-                setGamesReady(true);
-            }
-            else {
+        getGame()
+            .then(() => {
+                if (games.length > 0) {
+                    setGamesReady(true);
+                } else {
+                    setGamesReady(false);
+                }
+            })
+            .catch((err) => {
+                setLoadError(err.message);
                 setGamesReady(false);
-            }
-        }).catch((err) => {
-            setLoadError(err.message);
-            setGamesReady(false);            
-        });
+            });
     }, [getGame, games]);
 
     return (
